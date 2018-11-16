cc9323f421b5bd181472140c60bf52e2cb28db67
Kristijan <kristija.junuzovski@codelogiq.com>
2018-11-16 13:58:11 +0100

    Updates

---
+++ precache-manifest.b81b0e6d0cee8c015af6920d7bb2ec3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2921c3d3b929f31c8b57",
+    "url": "/redux-crud/static/js/runtime~main.2921c3d3.js"
+  },
+  {
+    "revision": "8e66eecae4edfa2ee24a",
+    "url": "/redux-crud/static/js/main.8e66eeca.chunk.js"
+  },
+  {
+    "revision": "7344673f155b57169dc6",
+    "url": "/redux-crud/static/js/1.7344673f.chunk.js"
+  },
+  {
+    "revision": "8e66eecae4edfa2ee24a",
+    "url": "/redux-crud/static/css/main.49aeee7c.chunk.css"
+  },
+  {
+    "revision": "7646c2de7e5823ef76e82cd2ba3bddeb",
+    "url": "/redux-crud/index.html"
+  }
+];
